8e0716576eabbbf413268ba169da901b05c873c7
ChrisRixGames <68282488+ChrisRixGames@users.noreply.github.com>
2021-08-26 00:56:59 +0100

    Art and movement
    Added some early art
    
    Started some basic player movement, subject to change

---
+++ Let There Be Chaos/Assets/Scripts/IInteractable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInteractable
+{
+    void AddReagent(/*add reagent class*/);
+
+    void Interact();
+
+    void Finish();
+
+    void Fail();
+
+    float currentProgress
+    {
+        get;
+        set;
+    }
+
+    float endProgress
+    {
+        get;
+        set;
+    }
+
+    float failPoint
+    {
+        get;
+        set;
+    }
+}
---
+++ Let There Be Chaos/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed;
+    private Rigidbody2D rb2D;
+
+    private float horizontalMovement;
+    private float verticalMovement;
+
+    private void Awake()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+        verticalMovement = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb2D.AddForce(new Vector2(horizontalMovement * moveSpeed, verticalMovement * moveSpeed));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        
+    }
+}
